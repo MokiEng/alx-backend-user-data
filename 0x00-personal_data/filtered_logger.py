91a0b8a8edf86a3352c0e1df1be0af4b311fecaf
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-02 01:48:26 -0700

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,24 +62,31 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 def main():
     """Configure logging
     """
-    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
-    columns = fields.split(',')
-    query = "SELECT {} FROM users;".format(fields)
-    info_logger = get_logger()
-    connection = get_db()
-    with connection.cursor() as cursor:
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            record = map(
-                lambda x: '{}={}'.format(x[0], x[1]),
-                zip(columns, row),
-            )
-            msg = '{};'.format('; '.join(list(record)))
-            args = ("user_data", logging.INFO, None, None, msg, None, None)
-            log_record = logging.LogRecord(*args)
-            info_logger.handle(log_record)
+   logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
+    formatter = RedactingFormatter(fields=("name", "email", "phone", "ssn", "password"))
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    db = get_db()
+    if db is None:
+        return
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    for row in cursor:
+        log_data = "; ".join(
+            [f"{field}={formatter.REDACTION}" for field in logger.fields])
+        log_message = f"[HOLBERTON] user_data INFO {
+            row[6].strftime('%Y-%m-%d %H:%M:%S')}: {log_data};"
+        logger.info(log_message)
+
+    cursor.close()
+    db.close()
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class."""
