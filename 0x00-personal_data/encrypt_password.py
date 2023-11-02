24e4e9f8e19249fda88747982078dd6d2443b5b8
MokiEng <meconen20@gmail.com>
2023-11-02 08:43:22 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,7 +7,7 @@ def hash_password(password: str) -> bytes:
     """Generate a random salt."""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     return hashed_password
 
 
