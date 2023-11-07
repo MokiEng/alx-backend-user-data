46797104731c10200854b6b4e918ba00a31c9cbb
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-07 04:56:48 -0800

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -78,10 +78,23 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieves the user from a request.
-        """
+        """overloads Auth and retrieves the User instance for a request
         auth_header = self.authorization_header(request)
         b64_auth_token = self.extract_base64_authorization_header(auth_header)
         auth_token = self.decode_base64_authorization_header(b64_auth_token)
         email, password = self.extract_user_credentials(auth_token)
         return self.user_object_from_credentials(email, password)
+        """
+        if request is None:
+            return None
+        aut_header = self.authorization_header(request)
+        decoded_header = self.decode_base64_authorization_header(aut_header)
+
+        if not decoded_header:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_header)
+        if not user_email or not user_pwd:
+            return None
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
