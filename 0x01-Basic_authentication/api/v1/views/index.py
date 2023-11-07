6ed6dad244a2e7743d1f38e86f95db182939f436
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-07 05:28:56 -0800

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,14 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
+@app.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
     """GET /api/v1/unauthorized
-    Return:
-      - Unauthorized error.
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
-    """GET /api/v1/forbidden
-    Return:
-      - Forbidden error.
-    """
+    """GET /api/v1/forbidden."""
     abort(403)
