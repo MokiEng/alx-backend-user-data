43a4bc435dbd8939a7ed2c4601614645ae9f1237
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-09 09:09:35 -0800

    Update auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -38,8 +38,10 @@ class Auth:
         """
         return None
 
-def session_cookie(self, request=None) -> str:
+    def session_cookie(self, request=None) -> str:
         """Gets the value of the cookie named SESSION_NAME."""
-        if request is not None:
-            cookie_name = os.getenv('SESSION_NAME')
-            return request.cookies.get(cookie_name)
+        if request is None:
+            return None
+
+        session_name = os.getenv("SESSION_NAME", "_my_session_id")
+        return request.cookies.get(session_name)
