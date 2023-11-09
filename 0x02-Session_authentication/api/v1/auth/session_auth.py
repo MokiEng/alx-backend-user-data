7fb13425f3aa61d03484c210584ceefa8e745ff7
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-09 08:53:07 -0800

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Session authentication module for the API.
-"""
+"""Session authentication module."""
 from uuid import uuid4
 from flask import request
 
@@ -9,20 +8,21 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class.
-    """
+    """A class for Session authentication."""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """Creates a session id for the user.
         """
-        if type(user_id) is str:
-            session_id = str(uuid4())
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+            session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Retrieves the user id of the user associated with
+        """Get the user id of the user associated with
         a given session id.
         """
         if type(session_id) is str:
