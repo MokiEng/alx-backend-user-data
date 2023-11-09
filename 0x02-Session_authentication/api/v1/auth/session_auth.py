be4ade4beb71c89feb315bca39f69e4742209083
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-09 09:04:32 -0800

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,8 +29,9 @@ class SessionAuth(Auth):
     def current_user(self, request=None) -> User:
         """Retrieves the user associated with the request.
         """
-        user_id = self.user_id_for_session_id(self.session_cookie(request))
-        return User.get(user_id)
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
     def destroy_session(self, request=None):
         """Destroys an authenticated session.
