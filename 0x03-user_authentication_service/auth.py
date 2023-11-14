e5f10633d5bd4f77c278174a45fec932c531e58a
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-14 10:38:44 -0800

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,16 +96,11 @@ class Auth:
     def update_password(self, reset_token: str, password: str) -> None:
         """Updates a user's password given the user's reset token.
         """
-        user = None
-        try:
-            user = self._db.find_user_by(reset_token=reset_token)
-        except NoResultFound:
-            user = None
-        if user is None:
-            raise ValueError()
-        new_password_hash = _hash_password(password)
-        self._db.update_user(
-            user.id,
-            hashed_password=new_password_hash,
-            reset_token=None,
-        )
+         user = self._db.find_user_by(reset_token=reset_token)
+        if not user:
+            raise ValueError("Invalid reset token")
+
+        hashed_password = self._hash_password(password)
+        user.hashed_password = hashed_password
+        user.reset_token = None
+        self._db.update_user(user)
