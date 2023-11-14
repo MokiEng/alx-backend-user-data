66ea78522fafc6a97bdd66f6464473ca1eb62e47
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-14 01:06:23 -0800

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,18 +29,18 @@ class Auth:
     """
 
     def __init__(self):
-        """Initializes a new Auth instance.
-        """
+        """Initializes a new Auth instance."""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Adds a new user to the database.
-        """
+        """Register a new user."""
         try:
             self._db.find_user_by(email=email)
-        except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
-        raise ValueError("User {} already exists".format(email))
+            raise ValueError(f"User {email} already exists")
+        except Exception as e:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """Checks if a user's login details are valid.
