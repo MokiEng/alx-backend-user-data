dfe4a6c6109980207fdbf7da44681d0fec3254dc
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-14 00:54:41 -0800

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,7 +14,7 @@ def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt.
     """
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hashed_password
 
 
