c5241359d6426474a044b5cb985615859aeb1176
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-15 11:10:26 -0800

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,40 +83,28 @@ def profile() -> str:
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
-    """POST /reset_password
-    Return:
-        - The user's password reset payload.
-    """
-    email = request.form.get("email")
-    reset_token = None
+    """POST /reset_password."""
+    email = request.form.get('email')
+
     try:
         reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
-        reset_token = None
-    if reset_token is None:
-        abort(403)
-    return jsonify({"email": email, "reset_token": reset_token})
+        return "Email not registered", 403
 
 
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
-    """PUT /reset_password
+    """PUT /reset_password."""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
 
-    Return:
-        - The user's password updated payload.
-    """
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        is_password_changed = True
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
-        is_password_changed = False
-    if not is_password_changed:
-        abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+        return "Invalid reset token", 403
 
 
 if __name__ == "__main__":
