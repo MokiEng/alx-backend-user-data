dcbe9fa5b1757427314082d86227472a0f9b2342
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-11-15 10:55:14 -0800

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,12 +58,16 @@ def logout() -> str:
     Return:
         - Redirects to home route.
     """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect('/')
+        else:
+            return "Forbidden", 403
+    else:
+        return "Forbidden", 403
 
 
 @app.route("/profile", methods=["GET"], strict_slashes=False)
